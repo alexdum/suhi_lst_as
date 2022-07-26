cab426210bea71741b981deeea9605f35351a186
alexdum <alexandru.dumitrescu@gmail.com>
2022-07-26 14:33:50 +0300

    improve visibility

--- ui.R
+++ ui.R
@@ -1,23 +1,42 @@
-library(shiny)
-library(shinythemes)
-library(highcharter)
-library(tidyr)
-library(dplyr)
 
+source("sections/ui_graphs.R", local = T)
 # Define UI for application that draws a histogram
-fluidPage(
-  sidebarLayout(
-    fluid = T,
-    
-    sidebarPanel(
-      width = 3,
-      selectInput("city", "City:", choices)
-    ),
-    mainPanel( 
-      width = 9,
-      fluidRow(
-        highchartOutput("chart2")
-      )
+
+ui <- shinyUI(
+  
+  ui <- function(req) { 
+    fluidPage(theme = shinytheme("spacelab"),
+              
+              tags$head(
+                
+                tags$style(
+                  type = "text/css",
+
+                  # "header {
+                  #   border: 1px solid blue;
+                  #   height: 150px;
+                  #   display: flex;                   /* defines flexbox */
+                  #     flex-direction: column;          /* top to bottom */
+                  #     justify-content: space-between;  /* first item at start, last at end */
+                  # }",
+                  # "section {
+                  #   border: 1px solid blue;
+                  #   height: 150px;
+                  #   display: flex;                   /* defines flexbox */
+                  #     align-items: flex-end;           /* bottom of the box */
+                  # }",
+                  "body {padding-top: 70px;}",
+                  #
+                )
+                
+              ),
+              useShinyjs(),
+              
+              navbarPage(
+                "SUHI explorer",    collapsible = T, fluid = T, id = "tabs", position =  "fixed-top",
+                
+                ui_graphs,
+              )
     )
-  )
+  }
 )
