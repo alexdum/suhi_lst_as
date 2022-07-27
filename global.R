1f96edbc73bbc822186b6b54fdf10d8347b1aa0a
alexdum <alexandru.dumitrescu@gmail.com>
2022-07-27 15:17:35 +0300

    adaugare orase

--- global.R
+++ global.R
@@ -6,5 +6,8 @@ library(dplyr)
 library(shinyjs)
 
 # listă orașe din tabel selectInput
-select_input_cities <- read.csv("www/data/tabs/select_input_cities.csv")
-choices <- setNames(select_input_cities$choice,select_input_cities$label)
+select_input_cities <- read.csv("www/data/tabs/select_input_cities.csv") %>%
+                     arrange(label) %>% filter(!label %in% c("Reykjavik", "Nur Sultan"))
+choices <- setNames(select_input_cities$choice,paste0(select_input_cities$label, " (", select_input_cities$country,")"))
+
+
