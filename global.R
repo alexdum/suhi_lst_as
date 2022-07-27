571967270f0b4244946cff14fa619d66a3e1aa1e
alexdum <alexandru.dumitrescu@gmail.com>
2022-07-27 17:39:47 +0300

    filrare orase

--- global.R
+++ global.R
@@ -5,9 +5,16 @@ library(tidyr)
 library(dplyr)
 library(shinyjs)
 
-# listă orașe din tabel selectInput
+
+cities <- list.files("www/data/tabs/suhi", pattern = "^suhi", full.names = T) %>%
+          strsplit(., "_|\\.") %>% do.call(rbind, .) %>% as_tibble() 
+
+
+# listă orașe din tabel selectInput care au date cities
 select_input_cities <- read.csv("www/data/tabs/select_input_cities.csv") %>%
-                     arrange(label) %>% filter(!label %in% c("Reykjavik", "Nur Sultan"))
+  arrange(label) %>% filter(!label %in% c("Reykjavik", "Nur Sultan")) %>% 
+  left_join(cities, by = c("choice" = "V2"))
+
 choices <- setNames(select_input_cities$choice,paste0(select_input_cities$label, " (", select_input_cities$country,")"))
 
 
