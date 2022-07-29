f93b68f728e38515ed5753f4efc5c4846b6f330b
alexdum <alexandru.dumitrescu@gmail.com>
2022-07-29 18:02:58 +0300

    use function

--- server.R
+++ server.R
@@ -4,35 +4,14 @@ server <- shinyServer(function(input, output, session) {
   
  
   
-  output$chart2 <- renderHighchart({
+  output$suhi <- renderHighchart({
     
     uhi_day <- read.csv(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv")) %>%
              mutate(date = as.Date(date))
     uhi_day.longf <- uhi_day %>% dplyr::select(date, uhi.max, uhi.min) %>%
       pivot_longer(!date, names_to = "area", values_to = "uhi") 
-    
-    hchart(
-      uhi_day.longf, "line",
-      hcaes(x = date, y = uhi, group = area) ,
-      color = c( "red", "blue")
-    ) %>%
-      hc_xAxis(
-        title = list(text = "Days") # Large bolded titles
-      ) %>%
-      hc_yAxis(
-        title = list(text = "SUHI [°C]") # Large bolded titles
-      ) %>% 
-      hc_navigator(
-        enabled = TRUE, baseSeries = 1
-      ) %>% 
-      hc_rangeSelector(
-        enabled = TRUE,
-        selected = 3
-      )  %>% 
-      hc_exporting(
-        enabled = TRUE, # always enabled
-        filename = paste0(input$city, "_uhi_lst_as")
-      )
+    #grafic cu suhi
+   hc_plot(uhi_day.longf , yaxis = "SUHI [°C]", filename_save =  paste0(input$city, "_uhi_lst_as"), cols =  c( "red", "blue"))
     
   })
   
