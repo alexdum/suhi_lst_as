a55b4aba2750ba3a45d276c865efe253fb893c59
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-06 21:22:03 +0300

    use data table

--- server.R
+++ server.R
@@ -19,12 +19,9 @@ server <- shinyServer(function(input, output, session) {
   
   
   datas <- reactive({
-    uhi <- read.csv(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv")) %>%
-      mutate(date = as.Date(date))
-    lst <- read.csv(paste0("www/data/tabs/suhi/stats_",input$city,"_v02.csv")) %>%
-      mutate(date = as.Date(date))
-    
-    datas <- left_join(uhi,lst, by =c("date", "time.uhi.min", "time.uhi.max", "uhi.med"))
+    uhi <- fread(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv"))[,c("date", "uhi.min", "uhi.max")]
+    lst <- fread(paste0("www/data/tabs/suhi/stats_",input$city,"_v02.csv"))[,c("date", "med.urb", "med.rur")]
+    datas <- merge(uhi, lst, by.x.y = "date")
     
     list(datas = datas)
   })
@@ -43,20 +40,20 @@ server <- shinyServer(function(input, output, session) {
       input = datas()$datas, xaxis_series = c("med.urb", "med.rur"), filename_save = paste0(input$city, "_lst"),
       cols =  c("#ef3b2c","#9ecae1"), names = c("Urban", "Rural"), ytitle = "LST [°C]"
     )
-
+    
   })
   
   
-
+  
   
   output$downloadData <- downloadHandler(
-  
-      filename = function() {
-        paste0('data-', input$city, '.csv')
-      },
-      content = function(con) {
-        write.csv(datas()$datas, con)
-      }
-    )
+    
+    filename = function() {
+      paste0('data-', input$city, '.csv')
+    },
+    content = function(con) {
+      write.csv(datas()$datas, con)
+    }
+  )
   
 })
