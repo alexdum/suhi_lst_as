153e2b0c7c43c95a2dd9cf9e2c5316669746cb10
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-06 15:36:37 +0300

    improved graphs

--- server.R
+++ server.R
@@ -6,14 +6,20 @@ server <- shinyServer(function(input, output, session) {
   
   output$suhi <- renderHighchart({
     
-    uhi_day <- read.csv(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv")) %>%
+    uhi <- read.csv(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv")) %>%
       mutate(date = as.Date(date))
-    uhi_day.longf <- uhi_day %>% dplyr::select(date, uhi.max, uhi.min) %>%
-      pivot_longer(!date, names_to = "area", values_to = "values") 
-    #grafic cu suhi
-    hc_plot(
-      input = uhi_day.longf , yaxis = "SUHI [°C]", filename_save =  paste0(input$city, "_uhi"), cols =  c( "red", "blue")
-    )
+    
+    #grafic cu lst
+    highchart(type = "stock")  |>
+      hc_xAxis(type = "datetime") |>
+      hc_add_series(uhi,type = "line", hcaes(date, uhi.max), color = "#800026", name = "SUHI Max") |>
+      hc_add_series(uhi,type = "line", hcaes(date, uhi.min), color = "#fd8d3c", name = "SUHI Min") |>
+      hc_legend(enabled = T) |>
+      hc_exporting(
+        enabled = TRUE, # always enabled
+        filename =  paste0(input$city, "_suhi")
+      )
+    
     
   })
   
@@ -21,16 +27,17 @@ server <- shinyServer(function(input, output, session) {
     
     lst <- read.csv(paste0("www/data/tabs/suhi/stats_",input$city,"_v02.csv")) %>%
       mutate(date = as.Date(date))
-    lst.longf <- lst %>% dplyr::select(date,  min.urb,     min.rur, max.urb,  max.rur) %>%
-      pivot_longer(!date, names_to = "area", values_to = "values") 
+ 
     #grafic cu lst
-    hc_plot(
-      input = lst.longf,
-      
-      yaxis = "LST [°C]",
-      filename_save = paste0(input$city, "_lst"), 
-      cols =  c( "#a50f15","#ef3b2c","#4292c6","#9ecae1")
-    )
+    highchart(type = "stock")  |>
+      hc_xAxis(type = "datetime") |>
+      hc_add_series(lst,type = "line", hcaes(date, med.urb), color = "#ef3b2c", name = "Urban") |>
+      hc_add_series(lst,type = "line", hcaes(date, med.rur), color = "#9ecae1", name = "Rural") |>
+      hc_legend(enabled = T) |>
+      hc_exporting(
+        enabled = TRUE, # always enabled
+        filename =  paste0(input$city, "_lst")
+      )
     
   })
   
