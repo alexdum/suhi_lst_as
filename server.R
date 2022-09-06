a6b137c0383bcb434decaa00165c8274401ed0f3
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-06 17:20:44 +0300

    add download data

--- server.R
+++ server.R
@@ -8,7 +8,6 @@ server <- shinyServer(function(input, output, session) {
     list(city = city)
   })
   
-  
   # titluri grafice
   output$text_uhi <- renderText({
     paste("Minimum and maximum Surface Urban Heat Island - ", text.cust()$city)
@@ -17,27 +16,47 @@ server <- shinyServer(function(input, output, session) {
     paste("Urban and Rural Land Surface Temperature - ", text.cust()$city)
   })
   
-  # grafice
-  output$suhi <- renderHighchart({
+  
+  
+  datas <- reactive({
     uhi <- read.csv(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv")) %>%
       mutate(date = as.Date(date))
+    lst <- read.csv(paste0("www/data/tabs/suhi/stats_",input$city,"_v02.csv")) %>%
+      mutate(date = as.Date(date))
+    
+    datas <- left_join(uhi,lst, by = c("date"))
+    
+    list(datas = datas)
+  })
+  # grafice
+  output$suhi <- renderHighchart({
+    
     
     hc_plot(
-      input = uhi, xaxis_series = c("uhi.max", "uhi.min"), filename_save = paste0(input$city, "_suhi"),
+      input = datas()$datas, xaxis_series = c("uhi.max", "uhi.min"), filename_save = paste0(input$city, "_suhi"),
       cols =  c("#800026","#fd8d3c" ), names = c("SUHI Max", "SUHI Min"), ytitle = "SUHI [°C]"
     )
   })
   
   output$lst <- renderHighchart({
-    
-    lst <- read.csv(paste0("www/data/tabs/suhi/stats_",input$city,"_v02.csv")) %>%
-      mutate(date = as.Date(date))
     hc_plot(
-      input = lst, xaxis_series = c("med.urb", "med.rur"), filename_save = paste0(input$city, "_lst"),
+      input = datas()$datas, xaxis_series = c("med.urb", "med.rur"), filename_save = paste0(input$city, "_lst"),
       cols =  c("#ef3b2c","#9ecae1"), names = c("Urban", "Rural"), ytitle = "LST [°C]"
     )
 
   })
   
+  
+
+  
+  output$downloadData <- downloadHandler(
+  
+      filename = function() {
+        paste0('data-', input$city, '.csv')
+      },
+      content = function(con) {
+        write.csv(datas()$datas, con)
+      }
+    )
   
 })
