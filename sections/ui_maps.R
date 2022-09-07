9b705668aeb2df538bf9c36a7b121a3b294ffed4
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-07 17:13:30 +0300

    ad maps section

---
+++ sections/ui_maps.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(shinythemes)
+library(highcharter)
+library(tidyr)
+library(dplyr)
+
+ui_maps <- tabPanel(
+  "Maps",value = "#maps", id = "#maps",icon = icon("map-location"),
+  #tags$br(""),
+  tabsetPanel( 
+    id = "tab maps",
+    tabPanel(
+      value = "suhi_maps",
+      title = "SUHI",
+      tags$h6(" "),
+      tags$h5("Cities for which SUHI has been calculated from LST AS SEVIRI product"),
+      #tags$h6(" "),
+      tags$br(""),
+      # sidebarLayout(
+      #   fluid = T,
+      #   sidebarPanel(
+      #     width = 3
+      #     
+      #   ),
+      #   mainPanel( 
+      #     width = 9,
+          fluidRow(
+            leafletOutput("map")
+          )
+    #     )
+    #   )
+    )
+  )
+)
+
+
