8c7f63165eadb3bf28f56dd38a71454ac0e696c7
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-07 21:56:48 +0300

    first commit

---
+++ sections/server_graphs.R
@@ -0,0 +1,46 @@
+text.cust <- reactive({
+  city <- select_input_cities$label[select_input_cities$choice == input$city]
+  list(city = city)
+})
+
+# titluri grafice
+output$text_uhi <- renderText({
+  paste("Minimum and maximum Surface Urban Heat Island - ", text.cust()$city)
+})
+output$text_lst <- renderText({
+  paste("Urban and Rural Land Surface Temperature - ", text.cust()$city)
+})
+
+datas <- reactive({
+  uhi <- fread(paste0("www/data/tabs/suhi/suhi_",input$city,"_v02.csv"))[,c("date", "uhi.min", "uhi.max")]
+  lst <- fread(paste0("www/data/tabs/suhi/stats_",input$city,"_v02.csv"))[,c("date", "med.urb", "med.rur")]
+  datas <- merge(uhi, lst, by.x.y = "date")
+  
+  list(datas = datas)
+})
+# grafice
+output$suhi <- renderHighchart({
+  
+  hc_plot(
+    input = datas()$datas, xaxis_series = c("uhi.max", "uhi.min"), filename_save = paste0(input$city, "_suhi"),
+    cols =  c("#800026","#fd8d3c" ), names = c("SUHI Max", "SUHI Min"), ytitle = "SUHI [°C]"
+  )
+})
+
+output$lst <- renderHighchart({
+  hc_plot(
+    input = datas()$datas, xaxis_series = c("med.urb", "med.rur"), filename_save = paste0(input$city, "_lst"),
+    cols =  c("#ef3b2c","#9ecae1"), names = c("Urban", "Rural"), ytitle = "LST [°C]"
+  )
+  
+})
+
+output$downloadData <- downloadHandler(
+  
+  filename = function() {
+    paste0('data-', input$city, '.csv')
+  },
+  content = function(con) {
+    write.csv(datas()$datas, con)
+  }
+)
