882e0d12dc597ae83c6136477d381b4a729ba86f
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-07 17:12:40 +0300

    add maps workfloe

--- global.R
+++ global.R
@@ -5,6 +5,9 @@ library(tidyr)
 library(dplyr)
 library(shinyjs)
 library(data.table)
+library(sf)
+library(leaflet)
+library(htmltools)
 
 source("utils/graphs_funs.R")
 
@@ -15,9 +18,9 @@ cities <- list.files("www/data/tabs/suhi", pattern = "^suhi", full.names = T) %>
 # listă orașe din tabel selectInput care au date cities
 select_input_cities <- read.csv("www/data/tabs/select_input_cities.csv") %>%
   arrange(label) %>% 
-  filter(!label %in% c("Reykjavik", "Nur Sultan", "Amman", "Monaco", "Beirut", "Baku","Citta di San Marino", "Vatican" )) %>% 
+  filter(!label %in% c("Reykjavik", "Nur Sultan", "Amman", "Monaco", "Beirut", "Baku","Citta di San Marino", "Vatican","Jerusalem" )) %>% 
   left_join(cities, by = c("choice" = "V2"))
 
 choices <- setNames(select_input_cities$choice,paste0(select_input_cities$label, " (", select_input_cities$country,")"))
 
-
+cities_map <- st_read("www/data/shp/cities_one_file.shp")
