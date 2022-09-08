ab439511fda5d0d1683a1dfab617dfbf99f61a7f
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-08 16:33:56 +0300

    change ids

--- sections/ui_maps.R
+++ sections/ui_maps.R
@@ -8,29 +8,39 @@ ui_maps <- tabPanel(
   "Maps",value = "#maps", id = "#maps",icon = icon("map"),
   #tags$br(""),
   tabsetPanel( 
-    id = "tab maps",
+    id = "tab_maps",
     tabPanel(
       value = "suhi_maps",
-      title = "SUHI",
+      title = "SUHI Map",
       tags$h6(" "),
       tags$h5("Cities for which SUHI has been calculated from LST AS SEVIRI product"),
       #tags$h6(" "),
       tags$br(""),
-      # sidebarLayout(
-      #   fluid = T,
-      #   sidebarPanel(
-      #     width = 3
-      #     
-      #   ),
-      #   mainPanel( 
-      #     width = 9,
+      sidebarLayout(
+        fluid = T,
+        sidebarPanel(
+          width = 3,
+          dateInput(
+            'days_suhi',
+            label = 'Select day',
+            min = min(dt.lst$date) |> as.Date(),
+            max = max(dt.lst$date) |> as.Date(),
+            value = max(dt.lst$date) |> as.Date()
+          )
+        )
+        ,
+        mainPanel(
+          width = 9,
           fluidRow(
             leafletOutput("map")
           )
-    #     )
-    #   )
+        )
+      )
     )
   )
 )
+
+
+
 
 
