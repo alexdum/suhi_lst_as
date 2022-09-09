f11d83e9f4688952da37211663fc29e262a91b11
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-09 15:13:16 +0300

    links to tabs

--- server.R
+++ server.R
@@ -1,7 +1,6 @@
-
-
 server <- shinyServer(function(input, output, session) {
   
+  source(file = "utils/tabs_url.R", local = T)
   # add sections
   source(file = "sections/server_graphs.R", local = T)
   source(file = "sections/server_map.R", local = T)
