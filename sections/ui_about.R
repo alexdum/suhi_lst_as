c68677dd8bd05264375416686927c6ae1d3306f7
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-10 21:31:16 +0300

    tidy code

--- sections/ui_about.R
+++ sections/ui_about.R
@@ -1,11 +1,13 @@
-ui_about <- tabPanel("About",icon = icon("info"), value = "#about", id = "#about",
-                     
-                    
-      fluidRow( h4("About"),
-      includeMarkdown("sections/about.md")
-      )
-                                     
-                                     
-                       
-                  
+ui_about <- tabPanel(
+  "About",icon = icon("info"), value = "#about", id = "#about",
+  
+  
+  fluidRow( 
+    h4("About"),
+    includeMarkdown("sections/about.md")
+  )
+  
+  
+  
+  
 )
