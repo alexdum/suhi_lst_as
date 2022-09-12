f7a8c735a24be20e630749c34bd762f143ee1930
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-12 21:28:27 +0300

    structure map tab

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ server <- shinyServer(function(input, output, session) {
   source(file = "utils/tabs_url.R", local = T)
   # add sections
   source(file = "sections/server_graphs.R", local = T)
-  source(file = "sections/server_map.R", local = T)
+  source(file = "sections/server_map_urban.R", local = T)
   
   # run code before accesing section
   outputOptions(output, "lst", suspendWhenHidden = FALSE)
