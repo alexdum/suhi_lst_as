dc1e902223463e34f11cd2bf28021ac74194ca4c
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-12 21:29:26 +0300

    add europe raster

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ server <- shinyServer(function(input, output, session) {
   # add sections
   source(file = "sections/server_graphs.R", local = T)
   source(file = "sections/server_map_urban.R", local = T)
+  source(file = "sections/server_map_europe.R", local = T)
   
   # run code before accesing section
   outputOptions(output, "lst", suspendWhenHidden = FALSE)
