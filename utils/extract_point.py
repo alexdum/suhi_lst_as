280a5e171c4c23939469938e05ef92fd1631d816
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-16 11:41:07 +0000

    extract  point data

---
+++ utils/extract_point.py
@@ -0,0 +1,9 @@
+import xarray as xr
+
+def extract_point(fname, lon, lat, variable):
+  fname = fname
+  ds = xr.open_dataset(fname)
+  ds.close()
+  dsloc = ds.sel(lon=lon,lat=lat,method='nearest')
+  data = dsloc[variable].to_pandas()
+  return(data)
