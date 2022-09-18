01cf861b57224998a8b7375f5f1b83dabb032a2d
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-18 13:36:35 +0300

    add option for one time series

--- utils/graphs_funs.R
+++ utils/graphs_funs.R
@@ -4,9 +4,11 @@ hc_plot <- function(input, xaxis_series, filename_save, cols, names, ytitle) {
   hh <-  
     highchart(type = "stock")  |>
     hc_xAxis(type = "date") |>
-    hc_add_series(input,type = "line", hcaes_string("date", xaxis_series[1]), color = cols[1], name = names[1]) |>
-    hc_add_series(input,type = "line", hcaes_string("date", xaxis_series[2]), color = cols[2], name = names[2]) |>
-    hc_legend(enabled = T) |>
+    hc_add_series(input,type = "line", hcaes_string("date", xaxis_series[1]), color = cols[1], name = names[1])
+    if (length(xaxis_series) == 2) {
+      hh <- hh |> hc_add_series(input,type = "line", hcaes_string("date", xaxis_series[2]), color = cols[2], name = names[2]) 
+    }
+  hh <- hh|> hc_legend(enabled = T) |>
     hc_exporting(
       enabled = TRUE, # always enabled
       filename =  filename_save) |>
