f4155cad83b583932ff363473e0cdeb6d644305b
alexdum <alexandru.dumitrescu@gmail.com>
2022-09-20 15:28:25 +0300

    add cities buff

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ server <- shinyServer(function(input, output, session) {
   
   source(file = "utils/tabs_url.R", local = T)
   # add sections
+  source(file = "sections/server_about.R", local = T)
   source(file = "sections/server_graphs.R", local = T)
   source(file = "sections/server_map_urban.R", local = T)
   source(file = "sections/server_map_europe.R", local = T)
