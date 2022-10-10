b7a064434f1fadebcfb9e45fead34829ed77cd97
alexdum <alexandru.dumitrescu@gmail.com>
2022-10-10 14:23:33 +0000

    update popup to use terra

--- utils/show_pop.R
+++ utils/show_pop.R
@@ -1,8 +1,8 @@
 #show raster values on the map
 show_pop <- function(x = NULL, y = NULL, rdat = NULL, proxy = NULL) {
-  cell <- cellFromXY(rdat, c(x, y))
-  if (!is.null(cell)) {
-    xy <- xyFromCell(rdat, cell)
+  cell <- terra::cellFromXY(rdat, cbind(x, y))
+  if (!is.na(cell)) {
+    xy <- terra::xyFromCell(rdat, cell)
     val = rdat[cell]
     if (!is.na(val)) proxy %>% clearPopups() %>% addPopups(xy[1],xy[2], popup = paste("LST: ", round(val,1)))
   }
