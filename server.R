d2c3d2b774a37e68b6a9d7fd568500a42924131c
alexdum <alexandru.dumitrescu@gmail.com>
2022-10-20 17:27:09 +0300

    change name daily

--- server.R
+++ server.R
@@ -5,7 +5,8 @@ server <- shinyServer(function(input, output, session) {
   source(file = "sections/server_about.R", local = T)
   source(file = "sections/server_graphs.R", local = T)
   source(file = "sections/server_map_urban.R", local = T)
-  source(file = "sections/server_map_europe.R", local = T)
+  source(file = "sections/server_map_europe_daily.R", local = T)
+  source(file = "sections/server_map_europe_monthly.R", local = T)
   
   # run code before accesing section
   outputOptions(output, "map.about", suspendWhenHidden = FALSE)
