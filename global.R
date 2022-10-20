0733398232d425b1928ea8b3b59769f8598685f1
alexdum <alexandru.dumitrescu@gmail.com>
2022-10-20 16:36:50 +0300

    update inputs indicators

--- global.R
+++ global.R
@@ -19,7 +19,7 @@ source("utils/graphs_funs.R")
 source("utils/show_pop.R")
 source_python("utils/extract_point.py") 
 
-width_panels <- c(3,8)
+width_panels <- c(2,7)
 
 # listă orașe din tabel selectInput care au date cities
 cities <- list.files("www/data/tabs/suhi", pattern = "^suhi", full.names = T) %>%
@@ -44,8 +44,10 @@ choices_map <- read.csv("www/data/tabs/slelect_input_parameters.csv")
 choices_map <-   setNames(choices_map$choice, choices_map$parameter)
 
 # pentru dropdown parameters maps europ
-choices_map_europe <- read.csv("www/data/tabs/slelect_input_parameters_europe.csv") 
-choices_map_europe <- setNames(choices_map_europe$choice, choices_map_europe$parameter)
+choices_map_europe_daily <- read.csv("www/data/tabs/slelect_input_parameters_europe_daily.csv") 
+choices_map_europe_daily <- setNames(choices_map_europe_daily$choice, choices_map_europe_daily$parameter)
+choices_map_europe_monthly <- read.csv("www/data/tabs/slelect_input_parameters_europe_monthly.csv") 
+choices_map_europe_monthly <- setNames(choices_map_europe_monthly$choice, choices_map_europe_monthly$parameter)
        
 
 # read all uhi files
@@ -60,7 +62,7 @@ dt.lst <- lapply(files.lst, fread)
 names(dt.lst) <- strsplit(files.lst, "stats_|_v") %>% do.call(rbind,.) %>% as_tibble() %>% dplyr::select(V2) %>% unlist()
 dt.lst <- rbindlist(dt.lst, idcol = "id" )
 
-# read daily lst
+# read ncs
 lst.max <- terra::rast("www/data/ncs/wmo_6_msg_lst_as_daily_max.nc")
 dats.lst.max  <- as.Date(names(lst.max) %>% gsub("MLST-AS_days=", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
 dats.lst.max <- dats.lst.max[dats.lst.max <=  max(dt.lst$date)]
@@ -73,6 +75,44 @@ lst.min <- terra::rast("www/data/ncs/wmo_6_msg_lst_as_daily_min.nc")
 dats.lst.min  <- as.Date(names(lst.min) %>% gsub("MLST-AS_days=", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
 dats.lst.min <- dats.lst.max[dats.lst.min <=  max(dt.lst$date)]
 
-    
+lst.mm <- terra::rast("www/data/ncs/wmo_6_msg_lst_as_daily_dineof_tmm.nc")
+dats.lst.mm <- as.Date(names(lst.mm) %>% gsub("MLST-AS_days=", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+
+lst.mn <- terra::rast("www/data/ncs/wmo_6_msg_lst_as_daily_dineof_tmn.nc")
+dats.lst.mn <- as.Date(names(lst.mn) %>% gsub("MLST-AS_days=", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+
+lst.mx <- terra::rast("www/data/ncs/wmo_6_msg_lst_as_daily_dineof_tmx.nc")
+dats.lst.mx <- as.Date(names(lst.mx) %>% gsub("MLST-AS_days=", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+
+
+# for mac
+if (Sys.info()[1] == "Darwin") {
+  # read daily lst
+  lst.maxx <- raster::stack("www/data/ncs/wmo_6_msg_lst_as_daily_max.nc") 
+  dats.lst.max  <- as.Date(names(lst.maxx) %>% gsub("X", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+  dats.lst.max <- dats.lst.max[dats.lst.max <=  max(dt.lst$date)]
+  lst.max <- rast(lst.maxx)
+  
+  lst.avgg <- raster::stack("www/data/ncs/wmo_6_msg_lst_as_daily_avg.nc")
+  dats.lst.avg  <- as.Date(names(lst.avgg) %>% gsub("X", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+  dats.lst.avg <- dats.lst.max[dats.lst.avg <=  max(dt.lst$date)]
+  lst.avg <- rast(lst.avgg)
+  
+  lst.minn <- raster::stack("www/data/ncs/wmo_6_msg_lst_as_daily_min.nc")
+  dats.lst.min  <- as.Date(names(lst.minn) %>% gsub("X", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+  dats.lst.min <- dats.lst.max[dats.lst.min <=  max(dt.lst$date)]
+  lst.min <- rast(lst.minn)
+  
+  lst.mavg <- raster::stack("www/data/ncs/wmo_6_msg_lst_as_daily_dineof_tmm.nc")
+  dats.lst.mm <- as.Date(names(lst.mavg) %>% gsub("X", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+  lst.mm <- rast(lst.mavg)
+  lst.mmin <- raster::stack("www/data/ncs/wmo_6_msg_lst_as_daily_dineof_tmn.nc")
+  dats.lst.mn <- as.Date(names(lst.mmin) %>% gsub("X", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+  lst.mn <- rast(lst.mmin)
+  lst.mmax <- raster::stack("www/data/ncs/wmo_6_msg_lst_as_daily_dineof_tmx.nc")
+  dats.lst.mx <- as.Date(names(lst.mmax) %>% gsub("X", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") 
+  lst.mx <- rast(lst.mmax)
+  
+}
 
 
